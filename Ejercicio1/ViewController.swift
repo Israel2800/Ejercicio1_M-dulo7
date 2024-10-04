212b490d68153a6eabfa3123a24d91f727a3c9bd
Israel2800 <dady281100@gmail.com>
2024-10-04 18:41:28 -0500

    Initial Commit

---
+++ Ejercicio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio1
+//
+//  Created by Paola Delgadillo on 10/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
